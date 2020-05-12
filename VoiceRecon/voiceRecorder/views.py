3806fe0d89dcc4bc43c98fbdbf1cc03426a6db66
Luis pc <luisrodrigoserrano@gmail.com>
2020-05-12 20:22:34 +0200

    Ugly voice recorder

--- VoiceRecon/voiceRecorder/views.py
+++ VoiceRecon/voiceRecorder/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 # Create your views here.
 def index(request):
     return render(request, template_name='voiceRecorder/index.html')
 
 
 def record(request):
-    return HttpResponse("This will be the recording menu")
+    name = request.POST['nameInput']
+    return HttpResponse('I think we recognise you, '+name)
 
 
 def result(request):
